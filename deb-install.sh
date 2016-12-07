df3f476106ee77ded122fa17b4697cf46d2a80bd
Alvaro Moran <alvaro.moran@gmail.com>
2016-12-07 15:14:13 +0100

    deb install added

---
+++ deb-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Few notes on packages installable in a Debian/Ubuntu environment
+
+sudo apt-get install vim-gtk cscope exuberant-ctags silversearcher-ag
+
+# Maybe these too
+
+sudo apt-get install terminator
