3b77cc6b0811744c8343579921bb16b744a9a957
Alvaro Moran <alvaro.moran@gmail.com>
2020-10-05 10:19:32 +0200

    deb-install: updated for ubuntu 20.04

--- deb-install.sh
+++ deb-install.sh
@@ -3,10 +3,8 @@
 # Few notes on packages installable in a Debian/Ubuntu environment
 
 # Many development packages, vim, python and clang.
-sudo apt-get install vim-gtk cscope exuberant-ctags \
-	uncrustify tmux build-essential cmake python3-dev \
-	clang curl libpython-dev
+sudo apt-get install vim-gtk3 cscope exuberant-ctags \
+	tmux build-essential cmake python3-dev \
+	clang curl libpython3-dev ripgrep
 
-# Maybe these too
-sudo snap install ripgrep ccls --classic
 
