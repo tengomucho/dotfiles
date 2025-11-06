7224a809f703a640ebc2a8da854eaa0b7b70841c
Alvaro Moran <alvaro.moran@gmail.com>
2025-11-06 18:30:16 +0100

    chore: small deb install update

--- deb-install.sh
+++ deb-install.sh
@@ -3,8 +3,8 @@
 # Few notes on packages installable in a Debian/Ubuntu environment
 
 # Many development packages, vim, python and clang.
-sudo apt-get install vim-gtk3 cscope exuberant-ctags \
+sudo apt-get install vim exuberant-ctags \
 	tmux build-essential cmake python3-dev \
-	clang curl libpython3-dev ripgrep
+	curl libpython3-dev ripgrep
 
 
