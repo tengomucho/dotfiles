cb7b633e6913cfc117641eef8b59def6ef570742
Alvaro Moran <alvaro@moran.it>
2026-07-30 11:54:36 +0200

    chore: remove atuin config change for now

--- home/run_once_install-atuin.sh
+++ home/run_once_install-atuin.sh
@@ -10,13 +10,13 @@ fi
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh -s -- --non-interactive
 
 # Disable atuin's up-arrow history search in favor of the default shell behavior
-for rc in "$HOME/.bashrc" "$HOME/.zshrc"; do
-    case "$rc" in
-        *.bashrc) shell=bash ;;
-        *.zshrc) shell=zsh ;;
-    esac
-    if [ -f "$rc" ]; then
-        echo "👋 After atuin's installation, you might want to run this command"
-        echo sed "s|^eval \"\$(atuin init $shell)\"\$|eval \"\$(atuin init $shell --disable-up-arrow)\"|" "$rc" > "$rc.tmp" && mv "$rc.tmp" "$rc"
-    fi
-done
+# for rc in "$HOME/.bashrc" "$HOME/.zshrc"; do
+#     case "$rc" in
+#         *.bashrc) shell=bash ;;
+#         *.zshrc) shell=zsh ;;
+#     esac
+#     if [ -f "$rc" ]; then
+#         echo "👋 After atuin's installation, you might want to run this command"
+#         echo sed "s|^eval \"\$(atuin init $shell)\"\$|eval \"\$(atuin init $shell --disable-up-arrow)\"|" "$rc" > "$rc.tmp" && mv "$rc.tmp" "$rc"
+#     fi
+# done
